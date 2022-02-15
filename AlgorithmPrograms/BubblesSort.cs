e86a9eefbdb7aaae4d7917e5ebfb583803c3d3d4
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-02-16 02:07:00 +0530

    Integer Bubble Sorting

---
+++ AlgorithmPrograms/BubblesSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class BubblesSort
+    {
+        public void Bubbles(int[] arr)
+        {
+            int num = arr.Length;
+            for (int i = 0; i < num-1; i++)
+            {
+                for (int j = 0; j<num-i-1;j++)
+                {
+                    if (arr[j]>arr[j+1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        public void Display(int[] arr)
+        {
+            int num = arr.Length;
+            for (int i =0; i<num;i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            Console.WriteLine("  ");
+        }
+
+    }
+}
--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -1,6 +1,6 @@
 ﻿using AlgorithmPrograms;
 Console.WriteLine("Hello, Welcome to Algorithm Programe...!");
-StringPermut stringPermut = new StringPermut();
+/*StringPermut stringPermut = new StringPermut();
 String str = "ABC";
 int n = str.Length;
 stringPermut.Permutation(str, 0, n - 1);
@@ -17,10 +17,16 @@ else
     Console.WriteLine("Found " + "at index " + results);
 }
 Insertation insertation = new Insertation();
-string[] names = { "ujj", "aru", "shi" };
+string[] names = { "ajj", "uru", "vhi" };
 insertation.InsertSort(names);
 foreach (var item in names)
 {
     Console.WriteLine(item);
-}
+}*/
+BubblesSort bubblesSort = new BubblesSort();
+int[] arry = { 1, 4, 3, 9, 5, 7 };
+bubblesSort.Bubbles(arry);
+Console.WriteLine("Your Sorted Array");
+bubblesSort.Display(arry);
+
 
