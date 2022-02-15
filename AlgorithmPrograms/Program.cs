1fbee6ba71ef77368f3bc84cb89aa842c9b1921f
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-02-16 02:21:51 +0530

    Checking Anagram

--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine("Hello, Welcome to Algorithm Programe...!");
 String str = "ABC";
 int n = str.Length;
 stringPermut.Permutation(str, 0, n - 1);
+Console.WriteLine("*******************************************");
 StringBinarys stringBinarys = new StringBinarys();
 String[] arr = { "LMO", "ABC", "DEF" };
 String x = "ABC";
@@ -16,17 +17,21 @@ else
 {
     Console.WriteLine("Found " + "at index " + results);
 }
+Console.WriteLine("*********************************************");
 Insertation insertation = new Insertation();
 string[] names = { "ajj", "uru", "vhi" };
 insertation.InsertSort(names);
 foreach (var item in names)
 {
     Console.WriteLine(item);
-}*/
+}
+Console.WriteLine("*********************************************");
 BubblesSort bubblesSort = new BubblesSort();
 int[] arry = { 1, 4, 3, 9, 5, 7 };
 bubblesSort.Bubbles(arry);
 Console.WriteLine("Your Sorted Array");
-bubblesSort.Display(arry);
+bubblesSort.Display(arry);*/
+Anagramss anagramss = new Anagramss();
+anagramss.Angram();
 
 
