46a59ee87f0c41197b1adcef20510c5eea41b78e
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-02-16 00:31:28 +0530

    String Permutation

--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AlgorithmPrograms;
+Console.WriteLine("Hello, Welcome to Algorithm Programe...!");
+StringPermut stringPermut = new StringPermut();
+String str = "ABC";
+int n = str.Length;
+stringPermut.Permutation(str, 0, n - 1);
+
---
+++ AlgorithmPrograms/StringPermut.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class StringPermut
+    {
+        public void Permutation(String str, int i, int j)
+        {
+            if (i == j)
+                Console.WriteLine("Your String is "+ str);
+            else
+            {
+                for (int i2 = 1; i2 <= j; i2++)
+                {
+                    str = Swap(str, i, i2);
+                    Permutation(str, i + 1, j);
+                    str = Swap(str, i, i2);
+                }
+            }
+        }
+        public static String Swap(String a, int m, int n)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[m];
+            charArray[m] = charArray[n];
+            charArray[n] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
